2e962829d8ab95d473efaef259dadd859ef969ce
Yuseok Jeon <jeon41@purdue.edu>
2020-12-23 19:09:43 +0900

    update llvm version

--- LLVM/build.sh
+++ LLVM/build.sh
@@ -2,10 +2,10 @@
 
 #get LLVM
 if [ ! -d llvm ]; then
-wget --retry-connrefused --tries=100 releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz
-tar -xf llvm-7.0.0.src.tar.xz
-mv llvm-7.0.0.src llvm
-rm llvm-7.0.0.src.tar.xz
+wget --retry-connrefused --tries=100 releases.llvm.org/10.0.0/llvm-10.0.0.src.tar.xz
+tar -xf llvm-10.0.0.src.tar.xz
+mv llvm-10.0.0.src llvm
+rm llvm-10.0.0.src.tar.xz
 
 pushd llvm/tools
 ln -s ../../clang .
@@ -14,18 +14,18 @@ fi
 
 #get Clang
 if [ ! -d clang ]; then
-wget --retry-connrefused --tries=100 releases.llvm.org/7.0.0/cfe-7.0.0.src.tar.xz
-tar -xf cfe-7.0.0.src.tar.xz
-mv cfe-7.0.0.src clang
-rm cfe-7.0.0.src.tar.xz
+wget --retry-connrefused --tries=100 releases.llvm.org/10.0.0/cfe-10.0.0.src.tar.xz
+tar -xf cfe-10.0.0.src.tar.xz
+mv cfe-10.0.0.src clang
+rm cfe-10.0.0.src.tar.xz
 fi
 
 #get compiler-rt
 if [ ! -d compiler-rt ]; then
-wget --retry-connrefused --tries=100 releases.llvm.org/7.0.0/compiler-rt-7.0.0.src.tar.xz
-tar -xf compiler-rt-7.0.0.src.tar.xz
-mv compiler-rt-7.0.0.src compiler-rt
-rm compiler-rt-7.0.0.src.tar.xz
+wget --retry-connrefused --tries=100 releases.llvm.org/10.0.0/compiler-rt-10.0.0.src.tar.xz
+tar -xf compiler-rt-10.0.0.src.tar.xz
+mv compiler-rt-10.0.0.src compiler-rt
+rm compiler-rt-10.0.0.src.tar.xz
 
 pushd llvm/projects
 ln -s ../../compiler-rt .
